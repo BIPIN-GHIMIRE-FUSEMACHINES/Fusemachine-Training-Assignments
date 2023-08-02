825a5a5b607a0f0e172628999ae6cb3878833137
Bipin Ghimire <bipin.ghimire@fusemachines.com>
2023-08-02 20:47:09 +0545

    feat: added division operation function

--- Week 1/Unit 1/calculator.py
+++ Week 1/Unit 1/calculator.py
@@ -7,6 +7,12 @@ def sub(a,b):
 def mul(a,b):
     print(f"{a} * {b} = ", a*b)
 
+def div(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    else:
+        print(f"{a} / {b} = ", a/b) 
+
 add(2,3)
 sub(2,3)
 mul(2,3)
