4518e15bd813d9234e4d1a29905c3ceb48678b71
Bipin Ghimire <bipin.ghimire@fusemachines.com>
2023-08-05 16:52:54 +0545

    feat: Completed part of assignment that calculated the product of elements from two list using list comprehension.

--- Week 1/Intermediate Python Programming- Unit 2/comprehension.py
+++ Week 1/Intermediate Python Programming- Unit 2/comprehension.py
@@ -9,3 +9,18 @@ original_list = ["barcelona", "madrid", "vigo", "city", "united", "miami", "suii
 filtered_list = [word for word in original_list if len(word) > 5]
 
 print(filtered_list)
+
+'''
+Question 2
+[list comprehension] Given two lists of integers, create a list that contains the
+product of each element of the first list with the corresponding element in the
+second list using list comprehension.
+'''
+
+list1 = [1, 2, 3, 4, 5,6,7,8]
+list2 = [10, 20, 30, 40, 50,60]
+
+# List comprehension to calculate the product of corresponding elements
+product_list = [list1[i] * list2[i] for i in range(min(len(list1), len(list2)))]
+
+print(product_list)
