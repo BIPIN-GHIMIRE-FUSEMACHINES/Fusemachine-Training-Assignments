ef7a929e388c4893fa55d93814b01b3bb4a4333b
Bipin Ghimire <bipin.ghimire@fusemachines.com>
2023-08-05 16:21:54 +0545

    feat: Added question2 of args that concatinates the given strings to single string

--- Week 1/Intermediate Python Programming- Unit 2/args_and_kwargs.py
+++ Week 1/Intermediate Python Programming- Unit 2/args_and_kwargs.py
@@ -27,3 +27,29 @@ print(sum_of_numbers(-1, -2, -3, -4, -5))
 print(sum_of_numbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))  
 print(sum_of_numbers(100))  
 print(sum_of_numbers())  
+
+
+'''
+Question 2
+[*args] Write a Python function concat_strings that takes any number of strings as
+arguments and returns a single concatenated string.
+'''
+def concat_strings(*args):
+    '''
+    Concatenate multiple strings into a single string.
+
+    Args:
+        *args: Any number of string arguments.
+
+    Returns:
+        str: A single string containing all the concatenated strings.
+    '''
+
+    return "".join(args)
+
+# Test cases
+print(concat_strings("Hello", " ", "fuse","human", "!"))  
+print(concat_strings("Working", " ", "with", " ", "*args and **kwargs."))  
+print(concat_strings("This", " ", "is", " ", "a", " ", "test."))  
+print(concat_strings("One", " ", "two", " ", "three"))  
+print(concat_strings("I", " ", "love", " ", "Barcelona", "!"))  
