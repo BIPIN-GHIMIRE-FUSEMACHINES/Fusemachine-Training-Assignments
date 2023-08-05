bd2496ff254dbe24cf608a12b5af4512be3f5885
Bipin Ghimire <bipin.ghimire@fusemachines.com>
2023-08-05 22:19:29 +0545

    Update: Updated the program convention according to pylint

--- Week 1/Intermediate Python Programming- Unit 2/University_OOP.py
+++ Week 1/Intermediate Python Programming- Unit 2/University_OOP.py
@@ -9,15 +9,33 @@ Department classes to display their details.
 '''
 
 class University:
+    """
+    Represents a University with attributes like name, location, and departments.
+
+    Attributes:
+        name (str): The name of the university.
+        location (str): The location of the university.
+        departments (list): A list of department objects.
+    """
+
     def __init__(self, name, location):
         self._name = name
         self._location = location
         self._departments = []
 
     def add_department(self, department):
+        """
+        Add a department to the university.
+
+        Args:
+            department (Department): The department object to add.
+        """
         self._departments.append(department)
 
     def display_details(self):
+        """
+        Display the details of the university.
+        """
         print(f"University Name: {self._name}")
         print(f"Location: {self._location}")
         print("Departments:")
@@ -26,6 +44,15 @@ class University:
 
 
 class Department(University):
+    """
+    Represents a Department within a university.
+
+    Attributes:
+        name (str): The name of the department.
+        head_of_department (str): The head of the department.
+        courses_offered (list): A list of courses offered by the department.
+    """
+
     def __init__(self, name, head_of_department):
         super().__init__("Sample University", "Sample Location")
         self._name = name
@@ -33,12 +60,27 @@ class Department(University):
         self._courses_offered = []
 
     def add_course(self, course):
+        """
+        Add a course to the department.
+
+        Args:
+            course (str): The name of the course to add.
+        """
         self._courses_offered.append(course)
 
     def get_name(self):
+        """
+        Get the name of the department.
+
+        Returns:
+            str: The name of the department.
+        """
         return self._name
 
     def display_details(self):
+        """
+        Display the details of the department.
+        """
         print(f"Department Name: {self._name}")
         print(f"Head of Department: {self._head_of_department}")
         print("Courses Offered:")
